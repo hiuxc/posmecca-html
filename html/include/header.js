6da0323e384b2332a3b04766c8603c3ff5000339
Sangwoo Byun <bsw@cntt.co.kr>
2024-03-19 17:52:54 +0900

    링크 수정

--- html/include/header.js
+++ html/include/header.js
@@ -9,10 +9,10 @@ function includeHeader() {
       </button>
       <ul>
         <li><a href="../html/service_solution.html" aria-label="Service">Service</a></li>
-        <li><a href="../htmlreference.html" aria-label="Reference">Reference</a></li>
-        <li><a href="../htmlesg_environment.html" aria-label="ESG">ESG</a></li>
-        <li><a href="../htmlcs_center_info.html" aria-label="CS Center">CS Center</a></li>
-        <li><a href="../htmlaboutus_greeting.html" aria-label="About us">About us</a></li>
+        <li><a href="../html/reference.html" aria-label="Reference">Reference</a></li>
+        <li><a href="../html/esg_environment.html" aria-label="ESG">ESG</a></li>
+        <li><a href="../html/cs_center_info.html" aria-label="CS Center">CS Center</a></li>
+        <li><a href="../html/aboutus_greeting.html" aria-label="About us">About us</a></li>
       </ul>
     </nav>
   </div>
